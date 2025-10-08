40fb1369d9ecb6c8714f01608678a74074b01e5a
Snehashish Mandal <snehashishmandal2004@gmail.com>
2025-10-08 14:21:55 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import DigitalTwinDashboard from '@/components/digital-twin-dashboard';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Home() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <main className="w-screen h-screen m-0 p-0">
-      <DigitalTwinDashboard />
+      {isClient ? <DigitalTwinDashboard /> : <div className="w-full h-full relative"><Skeleton className="w-full h-full" /></div>}
     </main>
   );
 }
